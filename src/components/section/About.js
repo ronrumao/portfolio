91f6027c973a478398c00e73c009f9f980fc82b5
Aaron Rumao <ronrumao@gmail.com>
2021-07-09 22:59:34 +0530

    code formatting changes

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -6,22 +6,22 @@ const About = () => {
     return (
         <div id="about" className="effect2">
             <div data-aos="zoom-in-up" data-aos-once="true" className="row">
-            <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
-            <div className="col-12 offset-md-1 col-md-6 about">
-                <div className="About-title-box">
-                <h1 id="About" className="red-line">{section2title}</h1>
+                <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
+                <div className="col-12 offset-md-1 col-md-6 about">
+                    <div className="About-title-box">
+                        <h1 id="About" className="red-line">{section2title}</h1>
+                    </div>
+                    <p className="lead about-text">
+                        {profile.personal.bio.short}
+                    </p>
                 </div>
-                <p className="lead about-text">
-                    {profile.personal.bio.short}  
-                </p>
             </div>
-            </div>
-            <div id="Skills"> 
+            <div id="Skills">
                 <div className="row d-flex justify-content-center skills">
-                    {profile.skills.skillsBar.map((x) => 
-                        <Skills faClass={x.faClass} label={x.name}/>
+                    {profile.skills.skillsBar.map((x) =>
+                        <Skills faClass={x.faClass} label={x.name} />
                     )}
-                </div> 
+                </div>
             </div>
         </div>
     )
